f7028178b088f00c2bbf79651ab421285b59882d
mingkuang <mingkuang@live.com>
2024-08-21 14:41:48 +0800

    添加jsoncpp-0.5.0

---
+++ Packages/jsoncpp/0.5.0/Pack.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module "..\..\public.psm1"
+
+$FileUrl = 'https://github.com/open-source-parsers/jsoncpp/archive/refs/tags/svn-release-0.5.0.zip'
+$PublishedHash = '2d920cda0cd44c43b513854c60a0c04928b5ed0dbb035a5f9909f7c33db1664f'
+$SourceRootName = 'jsoncpp-svn-release-0.5.0'
+
+SimplePack $FileUrl $PublishedHash $null $SourceRootName

52092f1547f6dc47eb838771bb42b8894f096a62
mingkuang <mingkuang@live.com>
2024-12-13 16:31:21 +0800

    添加 fmt 11.0.2

---
+++ Packages/fmt/11.0.2/Pack.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module "..\..\public.psm1"
+
+$FileUrl = 'https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.zip'
+$PublishedHash = '7aa4b58e361de10b8e5d7b6c18aebd98be1886ab3efe43e368527a75cd504ae4'
+$SourceRootName = 'fmt-11.0.2'
+
+SimplePack $FileUrl $PublishedHash $null $SourceRootName

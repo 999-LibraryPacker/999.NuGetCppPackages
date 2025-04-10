cc3485a68c3f904546fa7bd3033f280420cac05c
mingkuang <mingkuang@live.com>
2025-04-10 16:47:10 +0800

    添加 jsoncpp 1.9.5

---
+++ Packages/jsoncpp/1.9.5/Pack.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module "..\..\public.psm1"
+
+$FileUrl = 'https://github.com/open-source-parsers/jsoncpp/archive/refs/tags/1.9.5.zip'
+$PublishedHash = 'a074e1b38083484e8e07789fd683599d19da8bb960959c83751cd0284bdf2043'
+$SourceRootName = 'jsoncpp-1.9.5'
+
+SimplePack $FileUrl $PublishedHash $null $SourceRootName

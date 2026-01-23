206d9f01ecda5ece31b72379556055775c473b28
mingkuang <mingkuang@live.com>
2026-01-23 15:28:18 +0800

    添加jsoncpp 1.7.2

---
+++ Packages/jsoncpp/1.7.2/Pack.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module "..\..\public.psm1"
+
+$FileUrl = 'https://codeload.github.com/open-source-parsers/jsoncpp/zip/refs/tags/1.7.2'
+$PublishedHash = '90618516abaed488d23f7b7e358341075073cbdce3d1ed0329bb23cdaaa66183'
+$SourceRootName = 'jsoncpp-1.7.2'
+
+SimplePack $FileUrl $PublishedHash $null $SourceRootName
